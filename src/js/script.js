079b76cbc02f4e21814677f41b990f3cca4d0eb2
Yury <rz3ddd@gmail.com>
2020-05-30 20:14:26 +0300

    Refaactoring 01

--- src/js/script.js
+++ src/js/script.js
@@ -15,47 +15,66 @@ P.S. Функции вызывать не обязательно*/
 
 // Код возьмите из предыдущего домашнего задания
 
-const maxFilmNameLength = 50,
-    minFilmsClassicViewer = 3,
-    maxFilmsClassicViewer = 7;
+let personalMovieDB = startOfMovieRating();
 
-let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "0");
-if (numberOfFilms === 0 || isNaN(numberOfFilms) || numberOfFilms === undefined) {
-    alert(`Ваш ответ: ${numberOfFilms} \n Вы не можете участвовать в оценке фильмов!`);
-} else {
-    if (numberOfFilms < minFilmsClassicViewer) alert("Просмотрено довольно мало фильмов.");
-    else if (numberOfFilms <= maxFilmsClassicViewer) alert("Вы классический зритель.");
-    else alert("Вы киноман!");
+if (typeof personalMovieDB == "object") {
+    writeYourMovieScores(personalMovieDB);
+    writeYourGenres(personalMovieDB.genres);
+
+    showMyDB(personalMovieDB);
+} else alert("Error!");
+
+function showMyDB(objMyDB) {
+    'use strict';
+    if (!objMyDB.privat) console.log(objMyDB);
+    return;
+}
+
+function startOfMovieRating() {
+    'use strict';
+    const
+        minFilmsClassicViewer = 3,
+        maxFilmsClassicViewer = 7;
 
     let personalMovieDB = {
-        count: numberOfFilms,
+        count: 0,
         movies: {},
         actors: {},
         genres: [],
         privat: false,
     };
 
+    let numberOfFilms = getNumberAnswer("Сколько фильмов Вы уже посмотрели?");
+    if (numberOfFilms === 0) {
+        alert(`Ваш ответ: ${numberOfFilms} \n Вы не можете участвовать в оценке фильмов!`);
+        return false;
+    } else {
+        if (numberOfFilms < minFilmsClassicViewer) alert("Просмотрено довольно мало фильмов.");
+        else if (numberOfFilms <= maxFilmsClassicViewer) alert("Вы классический зритель.");
+        else alert("Вы киноман!");
+        personalMovieDB.count = numberOfFilms;
+        return personalMovieDB;
+    }
+}
+
+function writeYourMovieScores(personalMovieDB) {
+    'use strict';
+    const maxFilmNameLength = 50;
     let nextFilm = personalMovieDB.count,
         film = "",
         score = "";
+
     while (nextFilm) {
         film = getNonEmptyAnswer('Название следующего из последних просмотренных фильмов?', maxFilmNameLength);
         score = getNonEmptyAnswer('На сколько оцените его?', 3);
         personalMovieDB.movies[film] = score;
         nextFilm--;
     }
-
-    writeYourGenres(personalMovieDB.genres);
-
-    showMyDB(personalMovieDB);
-}
-
-function showMyDB(objMyDB) {
-    if (!objMyDB.privat) console.log(objMyDB);
     return;
 }
 
 function writeYourGenres(genres) {
+    'use strict';
     for (let i = 0; i < 3; i++) {
         genres[i] = getNonEmptyAnswer(`Ваш любимый жанр на ${i+1} месте?`, 24);
     }
@@ -63,6 +82,7 @@ function writeYourGenres(genres) {
 }
 
 function getNonEmptyAnswer(question, maxAnswerLength) {
+    'use strict';
     let answer = "";
     do {
         answer = prompt(question, '');
@@ -71,7 +91,17 @@ function getNonEmptyAnswer(question, maxAnswerLength) {
     return answer;
 }
 
+function getNumberAnswer(question) {
+    'use strict';
+    let answer = 0;
+    do {
+        answer = +prompt(question, '');
+    } while (answer == "" || answer == null || isNaN(answer));
+    return answer;
+}
+
 function normalizationOfSpaces(str) {
+    'use strict';
     let sourceStr = str.trim(),
         destStr = "",
         nextChar = "",
