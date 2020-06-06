680cabaa5e0c6440230ddbf99ed3c1f7f94feb18
Yury <rz3ddd@gmail.com>
2020-06-06 22:06:54 +0300

    Finish practice part 4

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,29 @@
 /* jshint esversion: 6 */
 
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
+/* Задание на урок: 1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 // Код возьмите из предыдущего домашнего задания
 
 let personalMovieDB = startOfMovieRating();
 
 if (typeof personalMovieDB == "object") {
-    writeYourMovieScores(personalMovieDB);
-    writeYourGenres(personalMovieDB.genres);
+    personalMovieDB.writeMovieScores();
+    personalMovieDB.writeGenres();
 
-    showMyDB(personalMovieDB);
+    personalMovieDB.genres.forEach((item, index) => {
+        console.log(`Любимый жанр №${index+1} это - ${item}`);
+    });
+
+    personalMovieDB.showMovieDB();
 } else alert("Error!");
 
-function showMyDB(objMyDB) {
-    'use strict';
-    if (!objMyDB.privat) console.log(objMyDB);
-    return;
-}
 
 function startOfMovieRating() {
     'use strict';
@@ -42,9 +37,41 @@ function startOfMovieRating() {
         actors: {},
         genres: [],
         privat: false,
+
+        showMovieDB() {
+            if (!this.privat) console.log(this);
+            return;
+        },
+
+        toggleVisibleMyDB() {
+            this.privat = !this.privat;
+        },
+
+        writeMovieScores() {
+            const maxFilmNameLength = 50;
+            let nextFilm = this.count,
+                film = "",
+                score = "";
+
+            while (nextFilm) {
+                film = getNonEmptyAnswer('Название следующего из последних просмотренных фильмов?', maxFilmNameLength);
+                score = getNumberAnswer('На сколько оцените его?', 3);
+                this.movies[film] = score;
+                nextFilm--;
+            }
+            return;
+        },
+
+        writeGenres() {
+            for (let i = 0; i < 3; i++) {
+                this.genres[i] = getNonEmptyAnswer(`Ваш любимый жанр на ${i+1} месте?`, 24);
+            }
+            return;
+        }
+
     };
 
-    let numberOfFilms = getNumberAnswer("Сколько фильмов Вы уже посмотрели?");
+    let numberOfFilms = getNumberAnswer("Сколько фильмов Вы уже посмотрели?", 0);
     if (numberOfFilms === 0) {
         alert(`Ваш ответ: ${numberOfFilms} \n Вы не можете участвовать в оценке фильмов!`);
         return false;
@@ -57,29 +84,7 @@ function startOfMovieRating() {
     }
 }
 
-function writeYourMovieScores(personalMovieDB) {
-    'use strict';
-    const maxFilmNameLength = 50;
-    let nextFilm = personalMovieDB.count,
-        film = "",
-        score = "";
 
-    while (nextFilm) {
-        film = getNonEmptyAnswer('Название следующего из последних просмотренных фильмов?', maxFilmNameLength);
-        score = getNonEmptyAnswer('На сколько оцените его?', 3);
-        personalMovieDB.movies[film] = score;
-        nextFilm--;
-    }
-    return;
-}
-
-function writeYourGenres(genres) {
-    'use strict';
-    for (let i = 0; i < 3; i++) {
-        genres[i] = getNonEmptyAnswer(`Ваш любимый жанр на ${i+1} месте?`, 24);
-    }
-    return;
-}
 
 function getNonEmptyAnswer(question, maxAnswerLength) {
     'use strict';
@@ -91,11 +96,11 @@ function getNonEmptyAnswer(question, maxAnswerLength) {
     return answer;
 }
 
-function getNumberAnswer(question) {
+function getNumberAnswer(question, defaultNumber) {
     'use strict';
     let answer = 0;
     do {
-        answer = +prompt(question, '');
+        answer = +prompt(question, defaultNumber);
     } while (answer == "" || answer == null || isNaN(answer));
     return answer;
 }
